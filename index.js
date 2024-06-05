769f73625f5aa002f1c91a7d3502198c5de641bd
tomielprodelsoftware <t.fuentes@alumno.etec.um.edu.ar>
2024-06-05 11:57:49 -0300

    commit

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ app.get('/kkk', (req, res) => {
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
+
+app.get('/kkk/:nombre', (req, res) => {
+  const nombre = req.params.nombre; 
+  res.send('Yo modifique este archivo :) ');
+});
